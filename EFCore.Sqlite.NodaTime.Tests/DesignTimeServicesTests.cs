0ba52ef2ca6656a6b9f7b024d94a1a1a762fef41
Kristian Hellang <kristian@hellang.com>
2020-12-16 21:25:38 +0100

    Add design-time services test

---
+++ EFCore.Sqlite.NodaTime.Tests/DesignTimeServicesTests.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore.Scaffolding;
+using Microsoft.EntityFrameworkCore.Sqlite.Design.Internal;
+using Microsoft.EntityFrameworkCore.Sqlite.Scaffolding.Internal;
+using Microsoft.EntityFrameworkCore.Sqlite.Storage.Internal;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Microsoft.EntityFrameworkCore.Sqlite
+{
+    public class DesignTimeServicesTests
+    {
+        [Fact]
+        public void ConfigureDesignTimeServices_works()
+        {
+            var serviceCollection = new ServiceCollection();
+            new SqliteNodaTimeDesignTimeServices().ConfigureDesignTimeServices(serviceCollection);
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            Assert.IsType<SqliteNodaTimeTypeMappingSourcePlugin>(serviceProvider.GetService<IRelationalTypeMappingSourcePlugin>());
+            Assert.IsType<SqliteNodaTimeCodeGeneratorPlugin>(serviceProvider.GetService<IProviderCodeGeneratorPlugin>());
+        }
+    }
+}
