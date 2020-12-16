6f5341a24463e12a190635e9c842cc76ae3ef84d
Kristian Hellang <kristian@hellang.com>
2020-12-16 20:55:14 +0100

    Add UseNodaTime extension method

---
+++ EFCore.Sqlite.NodaTime/Extensions/SqliteNodaTimeDbContextOptionsBuilderExtensions.cs
@@ -0,0 +1,26 @@
+using System;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Sqlite.Infrastructure.Internal;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.EntityFrameworkCore
+{
+    public static class SqliteNodaTimeDbContextOptionsBuilderExtensions
+    {
+        public static SqliteDbContextOptionsBuilder UseNodaTime(this SqliteDbContextOptionsBuilder builder)
+        {
+            if (builder is null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            var coreBuilder = ((IRelationalDbContextOptionsBuilderInfrastructure)builder).OptionsBuilder;
+
+            var extension = coreBuilder.Options.FindExtension<SqliteNodaTimeOptionsExtension>() ?? new SqliteNodaTimeOptionsExtension();
+
+            ((IDbContextOptionsBuilderInfrastructure)coreBuilder).AddOrUpdateExtension(extension);
+
+            return builder;
+        }
+    }
+}
---
+++ EFCore.Sqlite.NodaTime/Infrastructure/Internal/SqliteNodaTimeOptionsExtension.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Sqlite.Storage.Internal;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.EntityFrameworkCore.Sqlite.Infrastructure.Internal
+{
+    public class SqliteNodaTimeOptionsExtension : IDbContextOptionsExtension
+    {
+        private DbContextOptionsExtensionInfo? _info;
+
+        public DbContextOptionsExtensionInfo Info => _info ??= new ExtensionInfo(this);
+
+        public void ApplyServices(IServiceCollection services) => services.AddEntityFrameworkSqliteNodaTime();
+
+        public void Validate(IDbContextOptions options)
+        {
+            var provider = options.FindExtension<CoreOptionsExtension>()?.InternalServiceProvider;
+            if (provider is null)
+            {
+                return;
+            }
+
+            using var scope = provider.CreateScope();
+
+            var services = scope.ServiceProvider
+                .GetServices<IRelationalTypeMappingSourcePlugin>()
+                .OfType<SqliteNodaTimeTypeMappingSourcePlugin>();
+
+            if (services.Any())
+            {
+                throw new InvalidOperationException($"{nameof(SqliteNodaTimeDbContextOptionsBuilderExtensions.UseNodaTime)} requires {nameof(SqliteNodaTimeServiceCollectionExtensions.AddEntityFrameworkSqliteNodaTime)} to be called on the internal service provider used.");
+            }
+        }
+
+        private sealed class ExtensionInfo : DbContextOptionsExtensionInfo
+        {
+            public ExtensionInfo(IDbContextOptionsExtension extension)
+                : base(extension)
+            {
+            }
+
+            public override bool IsDatabaseProvider => false;
+
+            public override long GetServiceProviderHashCode() => 0;
+
+            public override void PopulateDebugInfo(IDictionary<string, string> debugInfo)
+                => debugInfo["Sqlite:" + nameof(SqliteNodaTimeDbContextOptionsBuilderExtensions.UseNodaTime)] = "1";
+
+            public override string LogFragment => "using NodaTime ";
+        }
+    }
+}
